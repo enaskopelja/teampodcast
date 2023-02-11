a90cc015e1ade915947e51149e40a92606e52040
Harmen van Rossum <harmen@cradle.bio>
2023-02-11 09:21:53 +0100

    added flask app

---
+++ dockers/docker-files/openai_summarize/app/app.py
@@ -0,0 +1,44 @@
+from http import HTTPStatus
+import os
+from flask import Flask, request, jsonify
+
+from gpt_index import GPTTreeIndex, SimpleDirectoryReader
+
+app = Flask(__name__)
+
+
+@app.route('/openai/summarize', methods = ['POST'])
+def summarize():
+    content = request.json
+    articles = content["articles"] # list of articles
+    prompts = content["prompts"] # list of prompts
+
+    # first make a directory data/ with all the articles as text files
+
+    # make the directory
+    os.mkdir("data")
+
+    # write the articles to the directory
+    for i, article in enumerate(articles):
+        with open(f"data/article_{i}.txt", "w") as f:
+            f.write(article["body"])
+
+    documents = SimpleDirectoryReader('data').load_data()
+    index = GPTTreeIndex(documents)
+    index.save_to_disk('index.json')
+
+    responses = []
+    for prompt in prompts:
+        responses.append({
+            "prompt" : prompt,
+            "response" : index.query(prompt)
+        })
+
+
+    return jsonify({
+        "statusCode" : HTTPStatus.ACCEPTED,
+        "body": {"responses" : responses}
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
